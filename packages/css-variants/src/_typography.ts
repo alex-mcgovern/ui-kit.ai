2cdf45d50a2b602e4d257064b2f2b068a3a5e4ed
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-09-21 00:25:28 +0100

    feat: typography package

---
+++ packages/css-variants/src/_typography.ts
@@ -0,0 +1,27 @@
+import type { ComplexStyleRule} from "@vanilla-extract/css";
+
+import { TypographyVariant } from "@boondoggle/css-types";
+import { vars } from "@boondoggle/css-vars";
+import { styleVariants } from "@vanilla-extract/css";
+
+type TypographyStyleVariants = Record<TypographyVariant, ComplexStyleRule>;
+
+/**
+ * A collection of variants that combine `font-size` and `line-height` styles.
+ */
+export const typography = styleVariants<TypographyStyleVariants>(
+	Object.values(TypographyVariant).reduce<TypographyStyleVariants>(
+		(variants, typographyVariant) => {
+			const fontSize = vars.font_size[typographyVariant];
+			const lineHeight = vars.line_height[typographyVariant];
+
+			variants[typographyVariant] = {
+				fontSize,
+				lineHeight,
+			};
+
+			return variants;
+		},
+		{} as TypographyStyleVariants,
+	),
+);
---
+++ packages/css-vars/src/index.ts
@@ -0,0 +1,13 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+import { FONT_SIZE, LINE_HEIGHT } from "./_typography";
+
+/**
+ * Global CSS variables attached to the `:root` selector.
+ * 
+ * @see https://vanilla-extract.style/documentation/global-api/create-global-theme/
+ */
+export const vars = createGlobalTheme(":root", {
+	font_size: FONT_SIZE,
+	line_height: LINE_HEIGHT,
+});
