6ed33da14230442484f5a057a9fed9381f39b83a
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-02-20 00:21:47 +0000

    chore(checkbox): mdx documentation

--- src/stories/checkbox-group.stories.tsx
+++ src/stories/checkbox-group.stories.tsx
@@ -1,31 +1,46 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Checkbox, CheckboxGroup } from "../components/checkbox";
-import React from "react";
+import React, { type ComponentProps } from "react";
 import { Label } from "../components/label";
 import { Description } from "../components/description";
+
+const Template = (args: ComponentProps<typeof CheckboxGroup>) => (
+    <CheckboxGroup defaultValue={["account-updates"]}>
+        <Label>Communication preferences</Label>
+        <Checkbox
+            value="account-updates"
+            isDisabled
+            isRequired
+            label="Account updates"
+            description="Necessary emails about your account & account security."
+        />
+        <Checkbox
+            value="newsletter"
+            label="Newsletter"
+            description="No more than one email per month with updates from our team."
+        />
+        <Checkbox
+            value="promotions"
+            label="Promotions and Offers"
+            description="Deals, discounts and suggestions we think you'll love."
+        />
+        <Description>
+            Your preferences can be updated at any time in your account
+            settings.
+        </Description>
+    </CheckboxGroup>
+);
 
 const meta = {
     title: "Components/CheckboxGroup",
     component: CheckboxGroup,
-    args: {
-        children: (
-            <>
-                <Label>This is the label</Label>
-                <Checkbox value="option_1" label="Option 1" />
-                <Checkbox value="option_2" label="Option 2" />
-                <Checkbox value="option_3" label="Option 3" />
-                <Description>This is the description</Description>
-            </>
-        ),
-    },
+    render: Template,
 } satisfies Meta<typeof CheckboxGroup>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const example: Story = {
-    args: {},
-};
+export const example: Story = {};
 export const isInvalid: Story = {
     args: {
         isInvalid: true,
