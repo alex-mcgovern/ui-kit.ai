801d1e4dfb4eb30c0cc0da622cd3b7a19cec5007
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-03-20 00:52:13 +0000

    begin building components metadata

---
+++ packages/metadata/src/utils/gen-prop-types.ts
@@ -0,0 +1,27 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { withCustomConfig } from "react-docgen-typescript";
+const outputPath = path.resolve(
+    import.meta.dirname,
+    "..",
+    "..",
+    "dist",
+    "prop-types.json",
+);
+
+function main() {
+    const resolvedPath = path.resolve(
+        `node_modules/@ui-kit.ai/components/dist/index.d.ts`,
+    );
+    const docs = withCustomConfig(path.resolve("./tsconfig.json"), {}).parse(
+        resolvedPath,
+    );
+    if (!Array.isArray(docs) || docs.length === 0) {
+        throw Error(`❌ Unable to parse typedefs for ${resolvedPath}`);
+    } else {
+        fs.writeFileSync(outputPath, JSON.stringify(docs, null, 2));
+        console.info(`✅  Component type data written to ${outputPath}`);
+    }
+}
+
+main();
---
+++ packages/metadata/src/utils/gen-usage-examples.ts
@@ -0,0 +1,67 @@
+import * as components from "@ui-kit.ai/storybook";
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+const outputPath = path.resolve(
+    import.meta.dirname,
+    "..",
+    "..",
+    "dist",
+    "usage-examples.json",
+);
+
+import { composeStories } from "@storybook/react";
+import React, { type ReactNode } from "react";
+import { type Options as ReactElementToJsxStringOptions } from "react-element-to-jsx-string";
+
+global.React = React;
+
+const JSX_STRING_OPTIONS = {
+    filterProps(_value, key) {
+        return key !== "data-testid" && key !== "key";
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    functionValue: (fn: Function) => fn.toString(),
+    showFunctions: true,
+    useBooleanShorthandSyntax: true,
+
+    useFragmentShortSyntax: true,
+} as const satisfies ReactElementToJsxStringOptions;
+
+async function main() {
+    const examples: Record<string, Record<string, string>> = {};
+
+    // NOTE: Dynamic import seems to help avoid "function does not exist" errors
+    const reactElementToJSXString = await (
+        await import("react-element-to-jsx-string")
+    ).default;
+
+    for (const [componentName, stories] of Object.entries(components)) {
+        try {
+            const composedStories = composeStories(
+                stories as Parameters<typeof composeStories>[0],
+                {
+                    applyDecorators: (storyFn) => storyFn, // We don't want to apply any decorators
+                },
+            );
+
+            const composed = Object.fromEntries(
+                Object.entries(composedStories).map(([storyName, Story]) => [
+                    storyName,
+                    reactElementToJSXString(
+                        (Story as () => ReactNode)(),
+                        JSX_STRING_OPTIONS,
+                    ),
+                ]),
+            );
+            examples[componentName] = composed;
+        } catch (e) {
+            console.error(`Error while processing ${componentName}: ${e}`);
+        }
+    }
+
+    fs.writeFileSync(outputPath, JSON.stringify(examples, null, 2), "utf-8");
+    console.info("✅ Component usage JSON generated successfully.");
+}
+
+main();
