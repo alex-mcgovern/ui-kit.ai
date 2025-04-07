df3f2ce5e39ed1b0233fdd5b4d304fdeb8a25f50
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-04-07 21:20:14 +0100

    document laoder component

---
+++ packages/storybook/src/stories/loader.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import type { ComponentProps } from 'react'
+
+import { Loader } from '@ui-kit.ai/components'
+
+function Template(args: ComponentProps<typeof Loader>) {
+    return <Loader {...args} />
+}
+
+const meta = {
+    component: Loader,
+    render: Template,
+    title: 'Components/Loader',
+} satisfies Meta<typeof Loader>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    parameters: {
+        displayName: 'Default',
+    },
+    storyName: 'Loader',
+}
+
+export const CustomSize: Story = {
+    args: {
+        className: 'size-20',
+    },
+    parameters: {
+        displayName: 'Custom Size',
+    },
+}
+
+export const CustomColor: Story = {
+    args: {
+        className: 'text-mid-contrast',
+    },
+    parameters: {
+        displayName: 'Custom Color',
+    },
+}
