cd208340dce29b0daa3b05cebb449322c0eb75b1
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-04-22 21:00:12 +0100

    feat(docs): display code result inline

--- packages/docs/src/components/code.tsx
+++ packages/docs/src/components/code.tsx
@@ -1,58 +1,32 @@
 'use client'
 
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  CodeBlock,
-  Tab,
-  TabList,
-  TabPanel,
-  Tabs,
-} from '@ui-kit.ai/components'
-import {
-  createContext,
-  type Dispatch,
-  type ReactNode,
-  type SetStateAction,
-} from 'react'
+import type { Dispatch, ReactNode, SetStateAction } from 'react'
 
-export const ShowCodeContext = createContext<
-  [boolean, Dispatch<SetStateAction<boolean>>] | null
->(null)
+import { Card, CardBody, CardHeader, CodeBlock } from '@ui-kit.ai/components'
+import { createContext } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export const ShowCodeContext = createContext<[boolean, Dispatch<SetStateAction<boolean>>] | null>(
+    null
+)
 
 export function Code({
-  className,
-  code,
-  component,
-  language = 'tsx',
+    className,
+    code,
+    component,
+    language = 'tsx',
 }: {
-  className?: string
-  code: string
-  component: ReactNode
-  language: string
+    className?: string
+    code: string
+    component: ReactNode
+    language: string
 }) {
-  return (
-    <Card className={className}>
-      <Tabs>
-        <CardHeader className='p-0'>
-          <TabList className='w-full'>
-            <Tab id='preview'>Preview</Tab>
-            <Tab id='code'>Code</Tab>
-          </TabList>
-        </CardHeader>
-        <CardBody>
-          <TabPanel
-            className='flex items-center justify-center min-h-24 bg-[radial-gradient(var(--color-tint-light)_1px,transparent_1px)] [background-size:16px_16px]'
-            id='preview'
-          >
-            {component}
-          </TabPanel>
-          <TabPanel id='code'>
-            <CodeBlock language={language}>{code}</CodeBlock>
-          </TabPanel>
-        </CardBody>
-      </Tabs>
-    </Card>
-  )
+    return (
+        <Card className={twMerge(className, 'bg-background')}>
+            <CardHeader className='border-b-tint-light'>{component}</CardHeader>
+            <CardBody className='bg-background-raised'>
+                <CodeBlock language={language}>{code}</CodeBlock>
+            </CardBody>
+        </Card>
+    )
 }
