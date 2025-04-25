583cb663ed9972811bf0d11eeb3c8eee4183435c
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-04-25 16:32:48 +0100

    feat: text area

---
+++ packages/storybook/src/stories/text-area.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { TextArea } from '@ui-kit.ai/components'
+
+const meta: Meta<typeof TextArea> = {
+    args: {
+        placeholder: 'This is a placeholder',
+    },
+    component: TextArea,
+    title: 'Components/TextArea',
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {},
+    parameters: {
+        displayName: 'Default',
+    },
+}
+
+export const Disabled: Story = {
+    args: {
+        ['aria-disabled']: true,
+    },
+    parameters: {
+        displayName: 'Disabled',
+    },
+}
+
+export const Invalid: Story = {
+    args: {
+        ['aria-invalid']: true,
+    },
+    parameters: {
+        displayName: 'Invalid',
+    },
+}
+
+export const Borderless: Story = {
+    args: {
+        isBorderless: true,
+    },
+    parameters: {
+        displayName: 'Borderless',
+    },
+}
