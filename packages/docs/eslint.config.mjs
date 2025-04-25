2a19d2db53b40d9018099b2b1735443459a0830f
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-04-25 15:49:53 +0100

    fix: eslint config type issue

--- packages/docs/eslint.config.mjs
+++ packages/docs/eslint.config.mjs
@@ -1,24 +1,23 @@
-import uiKitEslintConfig from "@ui-kit.ai/eslint";
+import uiKitEslintConfig from '@ui-kit.ai/eslint'
 
-/** @type {import('eslint').Linter.Config[]} */
 const config = [
     ...uiKitEslintConfig(import.meta.dirname),
     {
         ignores: [
-            ".turbo",
-            "dist",
-            "node_modules",
-            "public/mockServiceWorker.js",
-            "vite.config.mjs.timestamp-*.mjs",
+            '.turbo',
+            'dist',
+            'node_modules',
+            'public/mockServiceWorker.js',
+            'vite.config.mjs.timestamp-*.mjs',
         ],
     },
     // Ignore duplicate strings in stories
     {
-        files: ["**/*.stories.tsx"],
+        files: ['**/*.stories.tsx'],
         rules: {
-            "sonarjs/no-duplicate-string": "off",
+            'sonarjs/no-duplicate-string': 'off',
         },
     },
-];
+]
 
-export default config;
+export default config
