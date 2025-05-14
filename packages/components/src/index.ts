b47402eee4a3deac6d796a65212ca118f97efc39
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-05-14 21:25:53 +0100

    feat: implement autocomplete component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,6 @@
 import './index.css'
 export { Alert } from './components/alert'
+export { Autocomplete } from './components/autocomplete'
 export { Avatar } from './components/avatar'
 export { Button, LinkButton } from './components/button'
 export { Card, CardBody, CardFooter, CardHeader, CardLink, CardTitle } from './components/card'
