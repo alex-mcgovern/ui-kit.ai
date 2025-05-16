ea311071b704b70c42aaac85d1db928896aea876
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-05-16 15:43:29 +0100

    feat: add first-part `Link` component

---
+++ packages/components/src/components/link.tsx
@@ -0,0 +1,25 @@
+import type { ComponentProps } from 'react'
+
+import { Link as AriaLink } from 'react-aria-components'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * A link allows a user to navigate to another page or resource within a web page or application.
+ */
+export function Link(props: ComponentProps<typeof AriaLink>) {
+    return (
+        <AriaLink
+            {...props}
+            className={(renderProps) =>
+                twMerge(
+                    `text-dark hover:text-mid text-sm no-underline transition-colors hover:underline
+                    hover:decoration-dotted`,
+                    typeof props.className === 'function'
+                        ? props.className(renderProps)
+                        : props.className
+                )
+            }
+        />
+    )
+}
+Link.displayName = 'Link'
